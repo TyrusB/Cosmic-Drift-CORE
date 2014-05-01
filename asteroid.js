a3235f65f5ec81a373ef799dcb8c1a49982168be
TyrusB <brooks85.ty@gmail.com>
2014-05-01 13:51:19 -0700

    Add asteroid rotation. Fix asteroid spawning.

--- asteroid.js
+++ asteroid.js
@@ -3,6 +3,7 @@
 
   var Asteroid = Asteroids.Asteroid = function() {
     Asteroids.MovingObject.apply(this, arguments);
+    this.rotation = Math.random() * 360; 
   }
 
   Asteroid.inherits(Asteroids.MovingObject);
@@ -11,8 +12,8 @@
 
   Asteroid.RADIUS = 15;
 
-  Asteroid.MIN_RADIUS = 8;
-  Asteroid.MAX_RADIUS = 23;
+  Asteroid.MIN_RADIUS = 10;
+  Asteroid.MAX_RADIUS = 25;
 
   Asteroid.MAX_VEL = 3.5;
   Asteroid.MIN_VEL = 1.0;
@@ -46,20 +47,42 @@
   Asteroid.prototype.draw = function(ctx) {
     ctx.fillStyle = this.color;
 
+    var p1x = this.radius * Math.sin(this.rotation + 30 * (Math.PI / 180) );
+    var p1y = this.radius * Math.cos(this.rotation + 30 * (Math.PI / 180) );
+
+    var p2x = 1 / 2 * this.radius * Math.sin(this.rotation + 60 * (Math.PI / 180) );
+    var p2y = 1 / 2 * this.radius * Math.cos(this.rotation + 60 * (Math.PI / 180) );
+
+    var p3x = this.radius * Math.sin(this.rotation + 105 * (Math.PI / 180) );
+    var p3y = this.radius * Math.cos(this.rotation + 105 * (Math.PI / 180) );
+
+    var p4x = this.radius * Math.sin(this.rotation + 165 * (Math.PI / 180));
+    var p4y = this.radius * Math.cos(this.rotation + 165 * (Math.PI / 180));
+
+    var p5x = 2 / 3 * this.radius * Math.sin(this.rotation + 180 * (Math.PI / 180) );
+    var p5y = 2 / 3 * this.radius * Math.cos(this.rotation + 180 * (Math.PI / 180) );
+
+    var p6x = this.radius * Math.sin(this.rotation + 220 * (Math.PI / 180) );
+    var p6y = this.radius * Math.cos(this.rotation + 220 * (Math.PI / 180) );
+
+    var p7x = this.radius * Math.sin(this.rotation + 270 * (Math.PI / 180) );
+    var p7y = this.radius * Math.cos(this.rotation + 270 * (Math.PI / 180) );
+
+    var p8x = this.radius * Math.sin(this.rotation + 330 * (Math.PI / 180) );
+    var p8y = this.radius * Math.cos(this.rotation + 330 * (Math.PI / 180) );
+
     var x = this.posx,
-        y = this.posy,
-        r = this.radius * 2;
+        y = this.posy;
 
     ctx.beginPath()
-    ctx.moveTo(x + r / 4, y + r / 2);
-    ctx.lineTo(x + r / 3, y + r / 3);
-    ctx.lineTo(x + r / 2, y - r / 5);
-    ctx.lineTo(x + r / 4, y - r / 2);
-    ctx.lineTo(x, y - 3 * r / 8);
-    ctx.lineTo(x - 3 * r / 8, y - r / 2);
-    ctx.lineTo(x - 3 * r / 8, y - r / 3);
-    ctx.lineTo(x - r / 2, y);
-    ctx.lineTo(x - r / 3, y + r / 2);
+    ctx.moveTo(x + p1x, y + p1y);
+    ctx.lineTo(x + p2x, y + p2y);
+    ctx.lineTo(x + p3x, y + p3y);
+    ctx.lineTo(x + p4x, y + p4y);
+    ctx.lineTo(x + p5x, y + p5y);
+    ctx.lineTo(x + p6x, y + p6y);
+    ctx.lineTo(x + p7x, y + p7y);
+    ctx.lineTo(x + p8x, y + p8y);
     ctx.closePath();
     ctx.fill();
   }
